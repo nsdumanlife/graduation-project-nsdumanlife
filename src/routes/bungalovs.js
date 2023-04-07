323e3ee16783b16d0610af164f9667a486685e74
Numan Selman Duman <50591616+nsdumanlife@users.noreply.github.com>
2023-04-07 18:51:14 +0000

    Fix bungalovs routes after implementing mongoose

--- src/routes/bungalovs.js
+++ src/routes/bungalovs.js
@@ -31,24 +31,51 @@ router.get('/:bungalovName', function (req, res, next) {
 })
 
 // create a bungalov
-router.post('/', function (req, res, next) {
-  const user = User.list.find(user => user.name === req.body.user)
+router.post('/', async function (req, res, next) {
+  const user = await User.findById({ _id: req.body.user })
 
-  const bungalov = user.createBungalov(req.body.name, req.body.price, req.body.location)
+  const bungalov = await user.createBungalov(req.body.name, req.body.price, req.body.location)
 
-  res.send({ name: bungalov.name, price: bungalov.price, location: bungalov.location })
+  res.send(bungalov)
 })
 
 // delete a bungalov
-router.delete('/:bungalovName', function (req, res, next) {
-  const bungalov = Bungalov.list.find(bungalov => bungalov.name === req.params.bungalovName)
+router.delete('/:bungalovId', async function (req, res, next) {
+  const bungalov = await Bungalov.findByIdAndDelete(req.params.bungalovId)
 
   if (!bungalov) return res.status(404).send('Bungalov not found')
 
-  const bungalovIndex = Bungalov.list.indexOf(bungalov)
-  Bungalov.list.splice(bungalovIndex, 1)
+  res.sendStatus(200)
+})
 
-  res.send({ name: bungalov.name, price: bungalov.price, location: bungalov.location })
+// update a bungalov
+// router.put('/:bungalovName', function (req, res, next) {
+//   const bungalov = Bungalov.list.find(bungalov => bungalov.name === req.params.bungalovName)
+
+//   if (!bungalov) return res.status(404).send('Bungalov not found')
+
+//   bungalov.name = req.body.name
+//   bungalov.price = req.body.price
+//   bungalov.location = req.body.location
+
+//   res.send({ name: bungalov.name, price: bungalov.price, location: bungalov.location })
+// })
+
+// create a booking for a bungalov
+router.post('/:bungalovID/bookings', function (req, res, next) {
+  const bungalov = Bungalov.list.find(bungalov => bungalov.name === req.params.bungalovID)
+
+  if (!bungalov) return res.status(404).send('Bungalov not found')
+
+  const user = User.list.find(user => user.name === req.body.user)
+
+  const booking = user.book(bungalov, req.body.checkInDate, req.body.checkOutDate)
+
+  res.send({
+    bungalov: booking.bungalov.name,
+    checkInDate: booking.checkInDate,
+    checkOutDate: booking.checkOutDate,
+  })
 })
 
 module.exports = router
