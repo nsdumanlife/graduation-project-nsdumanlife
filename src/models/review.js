701dec34cf5a414c4e2f5c9379d782c2cfd0b188
Numan Selman Duman <50591616+nsdumanlife@users.noreply.github.com>
2023-04-11 13:15:50 +0000

    Fix timestamps property in review model
    Earlier there was an issue because of the curly braces, now it is fixed.

--- src/models/review.js
+++ src/models/review.js
@@ -1,30 +1,32 @@
 const mongoose = require('mongoose')
 
-const reviewSchema = new mongoose.Schema({
-  bungalov: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Bungalov',
-    autopopulate: {
-      maxDepth: 1,
+const reviewSchema = new mongoose.Schema(
+  {
+    bungalov: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Bungalov',
+      autopopulate: {
+        maxDepth: 1,
+      },
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+    },
+    text: String,
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      autopopulate: {
+        maxDepth: 1,
+      },
     },
   },
-  rating: {
-    type: Number,
-    min: 1,
-    max: 5,
-  },
-  text: String,
-  author: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    autopopulate: {
-      maxDepth: 1,
-    },
-  },
-  // {
-  //   timestamps: true,
-  // },
-})
+  {
+    timestamps: true,
+  }
+)
 
 class Review {}
 
