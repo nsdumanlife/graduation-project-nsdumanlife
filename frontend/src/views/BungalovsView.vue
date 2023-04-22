6ac6cc10ffc4e39875ad4ce894d40a7e16107d96
Numan Selman Duman <50591616+nsdumanlife@users.noreply.github.com>
2023-04-22 07:48:07 +0000

    Create bungalovs view, router path nad navbar link

---
+++ frontend/src/views/BungalovsView.vue
@@ -0,0 +1,28 @@
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'BungalovsView',
+  data() {
+    return {
+      bungalovs: []
+    }
+  },
+  async created() {
+    const { data: bungalovs } = await axios.get('http://localhost:3000/bungalovs')
+
+    this.bungalovs = bungalovs
+  }
+}
+</script>
+
+<template lang="pug">
+.bungalovs
+  h1 Bungalovs
+  ul
+    li(v-for='bungalov in bungalovs' :key='bungalov._id')
+      RouterLink(:to="`/bungalovs/${bungalov._id}`") {{ bungalov.name }} at {{ bungalov.location }} ${{ bungalov.price }}
+
+</template>
+
+<style></style>
