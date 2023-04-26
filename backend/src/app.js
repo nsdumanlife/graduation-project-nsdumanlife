cfa8d302e69c2b2f1c6767ceba1f0cf67941d58e
Numan Selman Duman <50591616+nsdumanlife@users.noreply.github.com>
2023-04-26 20:49:21 +0000

    Set sameSite none

--- backend/src/app.js
+++ backend/src/app.js
@@ -57,6 +57,7 @@ app.use(
     cookie: {
       secure: process.env.NODE_ENV === 'production',
       maxAge: 1000 * 60 * 60 * 24 * 15, // 15 days
+      sameSite: 'none',
     },
     store: MongoStore.create({ clientPromise, stringify: false }),
   })
